2a43356e620ae100034aa8c0ca8d40cedad3793c
김대철 <kimdaechul@gimdaecheol-ui-MacBookAir.local>
2022-09-05 11:56:16 +0900

    router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import ResultDetailPage from "./app/pages/ResultDetailPage";
+import ResultListPage from "./app/pages/ResultListPage";
 
 function App() {
   const [dataFormRN, setDataFromRN] = useState<string>();
@@ -35,24 +36,10 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>data from rn : {dataFormRN}</p>
-        <button onClick={sendMessage}>move navigator</button>
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    <Routes>
+      <Route path="/results" element={<ResultListPage />} />
+      <Route path="/detail" element={<ResultDetailPage />} />
+    </Routes>
   );
 }
 
---
+++ src/app/pages/ResultListPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ResultListPage() {
+  return <div>reuslt list page!</div>;
+}
+
+export default ResultListPage;
